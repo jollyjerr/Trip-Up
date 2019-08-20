889e6b0c2a408b50521e4b8cafaca7b8a1659217
jollyjerr <jollyjerr@gmail.com>
2019-08-20 13:11:01 -0600

    fixed bug when users destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 protect_from_forgery with: :exception
 before_action :current_user
-helper_method :current_user
+helper_method :current_user, :logged_in?
 
     def current_user
         @user = (User.find_by(id: session[:user_id]) || User.new)
