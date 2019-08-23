2801ff77f6429f10ad9dfd12645946abc71c737d
github username <github email address>
2019-08-23 11:23:34 -0600

    Fixes broken rspec tests

--- spec/features/trips_show_spec.rb
+++ spec/features/trips_show_spec.rb
@@ -8,7 +8,7 @@ describe 'Trips#show' do
     before(:each) do
         location = Location.create(city: "Denver", state: "NY")
         category = Category.create(name: "Hiking")
-        @trip = Trip.create!(id: 1, name: "Backpackin'", description: "Wooohooo", category_id: 1, location_id: 1)
+        @trip = Trip.create!(id: 1, name: "Backpackin'", description: "Wooohooo", category_id: 1, location_id: 1, date: "2019-08-24 12:30:00")
         user = User.create(id: 1, name: "Bob", email: "bobbyboy@gmail.com", password: "test", password_confirmation: "test")
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
         visit trip_path(@trip)
